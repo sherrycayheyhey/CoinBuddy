6c02fa91fac4062340da7ed7fefd5d42c105beef
sherrycayheyhey <sherrycaywood@yahoo.com>
2019-11-04 11:06:14 -0800

    Added gravity physics and touch action.

--- core/src/com/chromsicle/coinbuddy/game/CoinBuddy.java
+++ core/src/com/chromsicle/coinbuddy/game/CoinBuddy.java
@@ -12,6 +12,9 @@ public class CoinBuddy extends ApplicationAdapter {
 	Texture[] man; //uses an array because there's multiple sprites we want to show for him
 	int manState = 0; //this is for animating the different states
 	int pause = 0;
+	float gravity = 0.2f; //used for the gravity physics
+	float velocity = 0; //used for the gravity physics
+	int manY = 0; //y-position of the dude
 
 	//this is called when you open the game for the very first time
 	//it gets your game ready to go, things you only want to do one time
@@ -20,11 +23,16 @@ public class CoinBuddy extends ApplicationAdapter {
 		batch = new SpriteBatch();
 		//set up the background
 		background = new Texture("bg.png");
+
+		//set up the man frames
 		man = new Texture[4];
 		man[0] = new Texture("frame-1.png");
 		man[1] = new Texture("frame-2.png");
 		man[2] = new Texture("frame-3.png");
 		man[3] = new Texture("frame-4.png");
+
+		//set up the dude y-position value
+		manY = Gdx.graphics.getHeight() / 2;
 	}
 
 	//this gets run over and over again until the game is finished
@@ -35,6 +43,12 @@ public class CoinBuddy extends ApplicationAdapter {
 		//draw the background
 		//texture, xy position, length/width
 		batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+		//make him jump
+		//if the screen is touch, this will run once
+		if (Gdx.input.justTouched()) {
+            velocity = -10;
+        }
 
 		//the images cycle really fast so slow it
 		if (pause < 8) {
@@ -49,9 +63,19 @@ public class CoinBuddy extends ApplicationAdapter {
 			}
 		}
 
+		//gravity physics
+		velocity += gravity;
+		manY -= velocity;
+
+		//if the guy reaches the bottom, keep him there
+		if (manY <= 0) {
+			manY = 0;
+		}
+
+
 		//draw the coin dude
 		//to position it in the middle you have to / 2 but also account for his own dimensions
-		batch.draw(man[manState], Gdx.graphics.getWidth() / 2 - man[manState].getWidth() / 2, Gdx.graphics.getHeight() / 2);
+		batch.draw(man[manState], Gdx.graphics.getWidth() / 2 - man[manState].getWidth() / 2, manY);
 		batch.end();
 	}
 
